b8e27080c2d80739d79601ec6745ce5d816b01cd
Dillonso <dillonmuzondiwa@gmail.com>
2021-09-04 00:31:12 +0200

    Delete Untitled1.ps1

--- Untitled1.ps1
+++
@@ -1,2 +0,0 @@
-﻿curl -H 'Accept: application/json; indent=4' -u dispatch@booktime.domain:abcabcabc http://127.0.0.1:8000/api/orderlines/ -d '{"count": 2, "next": null, "previous":null, "results":[{"id":10, "order": "http://127.0.0.1:8000/api/orders/9/", "product": "Siddhartha","status": 10 }, {"id": 11, "order": "http://127.0.0.1:8000/api/orders/9/", "product": "Backgammon for dummies", "status":10}]}'
-    
